ddd3d7193a8d43badc9c0871eed458040cc236f8
ml2439 <ml2439@cornell.edu>
2017-06-26 16:57:26 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCA85bJDhP7zLSl4TdWWUV9Qw8QOsXmI4A",
+      authDomain: "crmlinkedin-f2f87.firebaseapp.com",
+      databaseURL: "https://crmlinkedin-f2f87.firebaseio.com",
+      projectId: "crmlinkedin-f2f87",
+      storageBucket: "crmlinkedin-f2f87.appspot.com",
+      messagingSenderId: "421112550030"
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!!!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Welcome to my crm!
         </Text>
       </View>
     );
