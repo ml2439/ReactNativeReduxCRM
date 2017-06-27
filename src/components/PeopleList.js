bc03fc77fc3cb044d4a260204dbb03b476d37911
ml2439 <ml2439@cornell.edu>
2017-06-27 09:38:34 -0400

    Add PersonItem and PeopleList view

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,16 +1,47 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  ListView,
+  StyleSheet
 } from 'react-native';
+import { connect } from 'react-redux';
+import PersonItem from './PersonItem';
 
-export default class PeopleList extends Component {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 353,
+    flexWrap: 'wrap',
+    paddingTop: 20,
+    paddingLeft: 20,
+  }
+})
 
+class PeopleList extends Component {
+  componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+    this.dataSource = ds.cloneWithRows(this.props.people);
+  }
   render() {
     return (
-      <View>
-          <Text>I See People</Text>
+      <View style={styles.container}>
+        <ListView 
+          enableEmptySections={true}
+          dataSource={this.dataSource}
+          renderRow={rowData => <PersonItem people={rowData} />}
+        />
       </View>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    people: state.people
+  }
+}
+
+export default connect(mapStateToProps)(PeopleList);
